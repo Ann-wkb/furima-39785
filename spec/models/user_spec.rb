f59e51a5d435c07c9c61937cbf4e8c2d1168fd0a
Ann-wkb <ano.wkb430@gmail.com>
2023-12-14 21:24:37 +0900

    validates

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe User, type: :model do
     context '新規登録できるとき' do
       it 'nicknameとemail、passwordとpassword_confirmation、first_nameとlast_name、first_name_kanaとlast_name_kanaとbirthdayが存在すれば登録できる' do
       end
+      it 'first_nameとlast_name、first_name_kanaとlast_name_kanaとbirthdayが存在すれば登録できる' do
+      end
     end
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
@@ -39,7 +41,33 @@ RSpec.describe User, type: :model do
       end
       it 'Passwordは、英字だけでは登録できない' do
       end
-      it 'Passwordは、数字だけでは登録できない' do
+      it 'first name（kana）は、半角では登録できない' do
+      end
+      it 'first name（kana）は、英語では登録できない' do
+      end
+      it 'first name（kana）は、数字では登録できない' do
+      end
+      it 'last name（kana）は、半角では登録できない' do
+      end
+      it 'lirst name（kana）は、英語では登録できない' do
+      end
+      it 'lirst name（kana）は、ひらがなでは登録できない' do
+      end
+      it 'first name（kana）は、ひらがなでは登録できない' do
+      end
+      it 'last name（kana）は、半角では登録できない' do
+      end
+      it 'first nameは、半角では登録できない' do
+      end
+      it 'first nameは、英語では登録できない' do
+      end
+      it 'first nameは、数字では登録できない' do
+      end
+      it 'last nameは、半角では登録できない' do
+      end
+      it 'lirst nameは、英語では登録できない' do
+      end
+      it 'lirst nameは、数字では登録できない' do
       end
     end
   end
