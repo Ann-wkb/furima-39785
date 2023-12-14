4057429bd8cb202176045812c8fa8af3367d5a97
Ann-wkb <ano.wkb430@gmail.com>
2023-12-14 21:56:23 +0900

    /\A[ぁ-んァ-ヶ一-龥々ー]+\z/

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,33 @@ RSpec.describe User, type: :model do
       it 'nicknameとemail、passwordとpassword_confirmation、first_nameとlast_name、first_name_kanaとlast_name_kanaとbirthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = 'abc12'
+        @user.password_confirmation = 'abc12'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = 'a12345'
+        @user.password_confirmation = 'a234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
     end
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
