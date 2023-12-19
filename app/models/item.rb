4f2b437f2d3200fffb706f322a6612626dfa8ef9
Ann-wkb <ano.wkb430@gmail.com>
2023-12-19 21:46:01 +0900

    models file

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,7 @@ class Item < ApplicationRecord
   # devise :database_authenticatable, :registerable,
   #      :recoverable, :rememberable, :validatable
   validates :image, presence: true
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
   validates :category_id, presence: true, numericality: { other_than: 0, message: 'Select' }
   validates :item_title, presence: true
   validates :item_description, presence: true
