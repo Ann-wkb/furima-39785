306c5e768cce3a7bd69ac21e290e51b3355ef2e8
Ann-wkb <ano.wkb430@gmail.com>
2023-12-19 16:41:30 +0900

    .toLocaleString();

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,14 @@
 window.addEventListener('turbo:load', () => {
-  console.log("OK");
 
   const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  console.log("イベント発火");
-});
 
-const taxPriceInput = document.getElementById("add-tax-price");
-console.log(taxPriceInput);
+    priceInput.addEventListener("input", () => {
 
-const profitInput = document.getElementById("profit");
-console.log(profitInput);
+      const addTaxDom = document.getElementById("add-tax-price");
+  // addTaxDom.innerHTML = "入力した金額をもとに販売手数料を計算する処理"
+      addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 ).toLocaleString();;
+
+      const addPofitDom = document.getElementById("profit");
+      addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 )).toLocaleString();
+    })
 });
