193b63b3181e32cbdbb3121c70834ef384ab79cf
Ann-wkb <ano.wkb430@gmail.com>
2023-12-19 17:53:32 +0900

    data: { turbo: false }

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -6,7 +6,7 @@ window.addEventListener('turbo:load', () => {
 
       const addTaxDom = document.getElementById("add-tax-price");
   // addTaxDom.innerHTML = "入力した金額をもとに販売手数料を計算する処理"
-      addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 ).toLocaleString();;
+      addTaxDom.innerHTML= Math.round(priceInput.value * 0.1 ).toLocaleString();;
 
       const addPofitDom = document.getElementById("profit");
       addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 )).toLocaleString();
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many_attached :images
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   extend ActiveHash::Associations::ActiveRecordExtensions
