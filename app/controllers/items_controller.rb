ddbb011582c48ea779fa78516ef622ba8ca18acb
Ann-wkb <ano.wkb430@gmail.com>
2023-12-21 19:23:39 +0900

    edititempath

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -18,21 +19,25 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      # @items = includes(:user)
       render :new, status: :unprocessable_entity
     end
   end
 
-  # def edit
-  # end
+  def edit
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
 
-  # def update
-  #   if @item.save
-  #     redirect_to item_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   #   if current_user.id == @item.user_id
@@ -47,5 +52,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_title, :item_description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                  :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
