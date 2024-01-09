074292c8f8a1fb6ce23559aaf8304c05abeff46d
Ann-wkb <ano.wkb430@gmail.com>
2024-01-09 15:48:05 +0900

    orderform

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -3,18 +3,18 @@ class OrderForm
 
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street, :building, :phone, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-
   with_options presence: true do
+    validates :user_id
+    validates :item_id
+
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :street
     validates :phone, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Enter a 10 to 11 digit number without hyphens' }
+
+    validates :token
   end
-  
-  validates :token, presence: true
   
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
